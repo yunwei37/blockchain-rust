0fc93b602e7cbd0339dfb20d2d20ef718fb32d8a
myub <1067852565@qq.com>
2020-08-31 07:44:39 -0700

    update part1

---
+++ README.md
@@ -0,0 +1,7 @@
+# blockchain-demo
+
+[![Actions Status](https://github.com/yunwei37/blockchain-demo/workflows/Build/badge.svg)](https://github.com/yunwei37/blockchain-demo/actions)
+
+reimplement blockchain_go in rust
+
+a simple blockchain for learning 
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ mod blockchain;
 pub type Result<T> = std::result::Result<T, failure::Error>;
 
 use blockchain::*;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() -> Result<()> {
     let mut bc = Blockchain::new();
+    sleep(Duration::from_millis(10));
     bc.add_block(String::from("Send 1 BTC to Ivan"))?;
-    bc.add_block(String::from("Send 1 BTC to Ivan"))?;
+    sleep(Duration::from_millis(30));
+    bc.add_block(String::from("Send 2 more BTC to Ivan"))?;
 
     println!("Blockchain: {:#?}", bc);
     Ok(())
